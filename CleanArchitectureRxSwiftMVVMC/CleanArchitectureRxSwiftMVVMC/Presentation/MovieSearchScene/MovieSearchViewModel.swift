605b33700bd1fd0038c35989a86c6eb723be74cd
jsim27 <simjh27@gmail.com>
2022-08-04 10:31:38 +0900

    feat: MovieSearchViewModel 타입 생성

---
+++ CleanArchitectureRxSwiftMVVMC/CleanArchitectureRxSwiftMVVMC/Presentation/MovieSearchScene/MovieSearchViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  MovieSearchViewModel.swift
+//  CleanArchitectureRxSwiftMVVMC
+//
+//  Created by Jae-hoon Sim on 2022/08/04.
+//
+
+import Foundation
+
+class MovieSearchViewModel: ViewModelProtocol {
+    struct Input {
+
+    }
+
+    struct Output {
+
+    }
+
+    func transform(input: Input) -> Output {
+
+        return Output()
+    }
+}
